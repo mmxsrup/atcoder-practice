e9c49930e6ef82cdaaddc4b9b792f4c85d540569
Yuichi Sugiyama <sugiyama98i@gmail.com>
2020-05-08 20:18:41 +0900

    Update abc 154 (D)

--- abc/0154/d/d.cpp
+++ abc/0154/d/d.cpp
@@ -7,12 +7,12 @@ int main(int argc, char const *argv[])
 {
 	int n, k; cin >> n >> k;
 	vector<int> p(n); rep(i, n) cin >> p[i];
-	vector<double> e(n + 1); rep(i, n) e[i] = (1. + p[i]) / 2.;
+	vector<double> e(n + 1); rep(i, n) e[i + 1] = (1. + p[i]) / 2.;
 	rep(i, n) e[i + 1] += e[i];
-	double ans = 0;
-	rep(i, n - k) {
+	double ans = 0.;
+	rep(i, n - k + 1) {
 		ans = max(ans, e[i + k] - e[i]);
 	}
-	printf("%.9f\n", ans);
+	printf("%.12f\n", ans);
 	return 0;
 }

bf809c107476a6f35b3a0c0cd74514eaee08ae9f
Yuichi Sugiyama <sugiyama98i@gmail.com>
2021-09-16 23:21:02 +0900

    Fix abc 178 (C)

--- abc/0178/c/c.cpp
+++ abc/0178/c/c.cpp
@@ -4,21 +4,18 @@ using namespace std;
 
 const ll MOD = 1e9 + 7;
 
+ll powmod(ll x, ll y) {
+	ll ret = 1;
+	for (ll i = 0; i < y; ++i) {
+		(ret *= x) %= MOD;
+	}
+	return ret;
+}
+
 int main(int argc, char const *argv[])
 {
-	int s; cin >> s;
-	int ans = 0;
-
-	for (int g = 1; g < 1000; ++g) {
-		int need = 3 * g;
-		if (need > s) {
-			break;
-		}
-		int amari = s - need;
-
-		// (amari + g)! / amari! g!
-		
-	}
-	cout << ans << endl;
+	ll n; cin >> n;
+	ll ans = powmod(10, n) - powmod(9, n) - powmod(9, n) + powmod(8, n);
+	cout << ans % MOD << endl;
 	return 0;
 }

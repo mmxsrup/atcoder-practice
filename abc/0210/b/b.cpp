7b23e96ad10770ab2afcbeaf26a347380f1379be
Yuichi Sugiyama <sugiyama98i@gmail.com>
2021-09-24 11:30:35 +0900

    Update abc 210 (B)

--- abc/0210/b/b.cpp
+++ abc/0210/b/b.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	int n; cin >> n;
-	string s; cin >> s;
+    int n; cin >> n;
+    string s; cin >> s;
 
-	for (int i = 0; i < (int)s.size(); ++i) {
-		if (s[i] == '1') {
-			cout << (i % 2 == 0 ? "Takahashi" : "Aoki") << endl;
-			break;
-		}
-	}
+    for (int i = 0; i < (int)s.size(); ++i) {
+        if (s[i] == '1') {
+            cout << (i % 2 == 0 ? "Takahashi" : "Aoki") << endl;
+            break;
+        }
+    }
     return 0;
 }
